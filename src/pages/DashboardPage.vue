8adb95f309a32a55551f7e2963e47cdf2070b33b
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2026-01-29 17:09:25 -0600

    cards wip

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -3,7 +3,18 @@
 </script>
 
 <template>
-    <div>
-        test
-    </div>
+    <UPageGrid>
+        <UPageColumns width="3/4">
+            <!-- big column -->
+             <UPageCard>
+
+             </UPageCard>
+        </UPageColumns>
+        <UPageColumns width="1/4">
+            <!-- small column -->
+             <UPageCard class="h-64">
+
+             </UPageCard>
+        </UPageColumns>
+    </UPageGrid>
 </template>
