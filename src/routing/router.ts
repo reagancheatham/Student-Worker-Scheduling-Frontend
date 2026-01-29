f3c95d96f9dda427655bb878472e78532ada2f1b
Davey Clonts <David.Clonts@eagles.oc.edu>
2026-01-29 15:34:05 -0600

    dont refactor rags code...

--- src/routing/router.ts
+++ src/routing/router.ts
@@ -1,7 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { Routes } from './routes.ts';
 
+import DashboardPage from "../pages/DashboardPage.vue"
+import OpenShiftsPage from "../pages/OpenShiftsPage.vue"
+import SchedulePage from "../pages/SchedulePage.vue"
+import SettingsPage from "../pages/SettingsPage.vue"
+
+const routes = [
+    { path: Routes.Dashboard, component: DashboardPage },
+    { path: Routes.Schedule, component: SchedulePage},
+    { path: Routes.OpenShifts, component: OpenShiftsPage},
+    { path: Routes.Settings, component: SettingsPage},
+]
+
 export const router = createRouter({
     history: createWebHistory(),
-    routes: Routes,
+    routes: routes,
 });
--- src/routing/routes.ts
+++ src/routing/routes.ts
@@ -1,12 +1,6 @@
-import DashboardPage from "../pages/DashboardPage.vue"
-import OpenShiftsPage from "../pages/OpenShiftsPage.vue"
-import SchedulePage from "../pages/SchedulePage.vue"
-import SettingsPage from "../pages/SettingsPage.vue"
-
-export const Routes = [
-    { path: '/dashboard', component: DashboardPage },
-    { path: '/schedule', component: SchedulePage},
-    { path: '/openShifts', component: OpenShiftsPage},
-    { path: '/settings', component: SettingsPage}
-]
-
+export const enum Routes {
+    Dashboard = "/dashboard",
+    Schedule = "/schedule",
+    OpenShifts = "/openShifts",
+    Settings = "/settings",
+}
