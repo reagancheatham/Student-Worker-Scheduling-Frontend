fe88d011d1ec610b5378875490e5efba4f32f30a
Davey Clonts <David.Clonts@eagles.oc.edu>
2026-01-30 15:52:22 -0600

    grid wip

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -3,12 +3,18 @@
 </script>
 
 <template>
-    <UPageGrid class="grid lg:grid-cols-4 gap-4">
+    <!-- Divides the page into 4ths -->
+    <div class="grid grid-cols-4 gap-4">
         <UPageGrid class="col-span-3">
+            <UPageCard class="col-span-full"></UPageCard>
+            <UPageCard></UPageCard>
+            <UPageCard></UPageCard>
 
         </UPageGrid>
-        <UPageCard class="col-span-1">
-
-        </UPageCard>
-    </UPageGrid>
+        
+        <!-- Notifs center -->
+        <UPageColumns>
+            <UPageCard></UPageCard>
+        </UPageColumns>
+    </div>
 </template>
