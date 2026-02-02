d139bb16698ed1ef1c806320f199455af4b15c54
Davey Clonts <David.Clonts@eagles.oc.edu>
2026-02-01 18:37:21 -0600

    basic grid finally

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -3,18 +3,18 @@
 </script>
 
 <template>
-    <!-- Divides the page into 4ths -->
-    <div class="grid grid-cols-4 gap-4">
-        <UPageGrid class="col-span-3">
-            <UPageCard class="col-span-full"></UPageCard>
-            <UPageCard></UPageCard>
-            <UPageCard></UPageCard>
-
-        </UPageGrid>
+    <div class="grid grid-cols-5 gap-4 h-full">
+        <!-- Big Side -->
+        <div class="col-span-4 grid grid-cols-2 grid-rows-5 gap-4">
+            <UPageCard class="col-span-2 row-span-3"></UPageCard>
+            <UPageCard class="row-span-2"></UPageCard>
+            <UPageCard class="row-span-2"></UPageCard>
+        </div>
         
-        <!-- Notifs center -->
-        <UPageColumns>
-            <UPageCard></UPageCard>
-        </UPageColumns>
+        <!-- Little Side -->
+         <div class="col-span-1">
+            <UPageCard class="h-full"></UPageCard>
+         </div>
     </div>
+
 </template>
