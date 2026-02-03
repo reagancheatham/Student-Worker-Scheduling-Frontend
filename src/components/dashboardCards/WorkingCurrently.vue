15ddff02c6064858b6c2da86e04c12ce906500d4
Davey Clonts <David.Clonts@eagles.oc.edu>
2026-02-03 16:35:17 -0600

    Working Currently Table

--- src/components/dashboardCards/WorkingCurrently.vue
+++ src/components/dashboardCards/WorkingCurrently.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import type { TableColumn } from "@nuxt/ui";
+import { h, resolveComponent } from "vue";
+
+const UBadge = resolveComponent("UBadge");
 
 type ShiftInfo = {
   Name: String;
@@ -13,16 +17,72 @@ const data = ref<ShiftInfo[]>([
     Shift: "10:30a.m. - 2:30p.m.",
     Status: "Late",
   },
+  {
+    Name: "David Clonts II",
+    Shift: "10:30a.m. - 2:30p.m.",
+    Status: "Late",
+  },
+  {
+    Name: "Reagan Cheatham",
+    Shift: "10:30a.m. - 2:30p.m.",
+    Status: "On Time",
+  },
+  {
+    Name: "Reagan Cheathamyie",
+    Shift: "10:30a.m. - 2:30p.m.",
+    Status: "On Time",
+  },
+  {
+    Name: "David Clonts",
+    Shift: "1:30a.m. - 4:30p.m.",
+    Status: "Upcoming",
+  },
 ]);
+
+const columns: TableColumn<ShiftInfo>[] = [
+  {
+    accessorKey: "Name",
+    header: "Name",
+    cell: ({ row }) => {
+      return `${row.getValue("Name")}`;
+    },
+  },
+  {
+    accessorKey: "Shift",
+    header: "Shift",
+    cell: ({ row }) => {
+      return `${row.getValue("Shift")}`;
+    },
+  },
+  {
+    accessorKey: "Status",
+    header: "Status",
+    cell: ({ row }) => {
+      const color = {
+        Late: "error" as const,
+        "On Time": "success" as const, //rag might not like this
+        Upcoming: "neutral" as const,
+      }[row.getValue("Status") as string];
+
+      //no idea what h is
+      return h(UBadge, { class: "capitalize", variant: "subtle", color }, () =>
+        row.getValue("Status"),
+      );
+    },
+  },
+];
 </script>
 
 <template>
-  <UCard :ui="{header: 'font-semibold text-black col-span-1', body: '!px-0 !pt-0 !pb-3'}">
-    <template #header> 
-      <span class="mb">
-        Working Currently
-      </span>
-  </template>
-    <UTable :data="data"> </UTable>
+  <UCard
+    :ui="{
+      header: 'font-semibold text-black col-span-1',
+      body: '!px-0 !pt-0 !pb-3',
+    }"
+  >
+    <template #header>
+      <span class="mb"> Working Currently </span>
+    </template>
+    <UTable :data="data" :columns="columns"> </UTable>
   </UCard>
 </template>
