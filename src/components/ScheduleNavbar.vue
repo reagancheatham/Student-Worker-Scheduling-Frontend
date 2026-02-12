788f32591f4a2695dc534de409144a7b7e24307c
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2026-02-12 17:31:01 -0600

    padding removed

--- src/components/ScheduleNavbar.vue
+++ src/components/ScheduleNavbar.vue
@@ -6,9 +6,12 @@
 <template>
     <UDashboardNavbar 
         :title="route.name"
+        :ui = "{
+            root: 'px-0!'
+        }"
     >
         <template #right>
-            <img class="h-60 w-auto" src="/OC.png" alt="OC Logo">
+            <img class="h-13 w-85" src="/OC.png" alt="OC Logo">
         </template>
     </UDashboardNavbar>
 </template>
