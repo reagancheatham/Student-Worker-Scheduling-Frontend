cdd871be558e7614b3950595c7b53da2cee6a582
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2026-02-19 17:33:21 -0600

    Employee class

---
+++ src/classes/Employee.ts
@@ -0,0 +1,11 @@
+class Employee {
+    ID: number;
+    Name: String;
+    Email: String;
+
+    constructor(ID: number, Name: String, Email: String) {
+        this.ID = ID;
+        this.Name = Name;
+        this.Email = Email;
+    };
+}
