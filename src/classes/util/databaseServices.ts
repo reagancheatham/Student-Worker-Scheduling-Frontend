5795430d5d3d159fc5022aa2b9b43b1f73deda23
reagancheatham <122314281+reagancheatham@users.noreply.github.com>
2026-02-27 14:08:47 -0600

    Create Database Services Class (#28)
    * Working on get function
    
    * Add database services

---
+++ src/classes/util/databaseServices.ts
@@ -0,0 +1,55 @@
+import { apiClient } from "../../services/services";
+
+export class DatabaseServices {
+    static async create(path: string, object: any) {
+        await apiClient
+            .post(path, object)
+            .then(() => {
+                console.log(`${path} created successfully`);
+            })
+            .catch((err) => {
+                console.error(`Error creating ${path}: ${JSON.stringify(err)}`);
+            });
+    }
+
+    static async update(path: string, object: any) {
+        await apiClient
+            .put(path, object)
+            .then(() => {
+                console.log(`${path} updated successfully`);
+            })
+            .catch((err) => {
+                console.error(`Error updating ${path}: ${JSON.stringify(err)}`);
+            });
+    }
+
+    static async delete(path: string) {
+        await apiClient
+            .delete(path)
+            .then(() => {
+                console.log(`${path} deleted successfully`);
+            })
+            .catch((err) => {
+                console.error(`Error deleting ${path}: ${JSON.stringify(err)}`);
+            });
+    }
+
+    static async get<T>(path: string): Promise<T> {
+        let finalResult: T | null = null;
+
+        await apiClient
+            .get(path)
+            .then((result) => {
+                finalResult = result as T;
+                console.log(`${path} found successfully`);
+            })
+            .catch((err) => {
+                console.error(`Error finding ${path}: ${JSON.stringify(err)}`);
+                throw err;
+            });
+
+        if (finalResult === null)
+            throw Error(`Error finding ${path}: invalid result!`);
+        else return finalResult;
+    }
+}
--- src/services/employeeServices.ts
+++ src/services/employeeServices.ts
@@ -1,61 +1,28 @@
 import { Employee } from "@classes/Employee";
-import { servicesUtil } from "@classes/util/servicesUtil";
-import { apiClient } from "./services";
+import { DatabaseServices } from "@classes/util/databaseServices";
 
 const API_ROOT: string = "employee";
 
-//TODO: CREATE PATHS FOR EACH GENERIC AND BUILD GET AND GETALL
 export class EmployeeServices {
-
     static async create(employee: Employee) {
-        await servicesUtil.create(API_ROOT, employee);
-    };
+        await DatabaseServices.create(API_ROOT, employee);
+    }
 
     static async update(employee: Employee) {
-        await servicesUtil.update(API_ROOT, employee);
-    };
+        await DatabaseServices.update(API_ROOT, employee);
+    }
 
     static async delete(employee: Employee) {
-        await servicesUtil.delete(`${API_ROOT}/:${employee.id}`);
-    };
+        await DatabaseServices.delete(`${API_ROOT}/:${employee.id}`);
+    }
 
     static async get(id: number) {
-        let employee: Employee;
+        return await DatabaseServices.get<Employee>(`${API_ROOT}/:${id}`);
+    }
 
-        await apiClient 
-            .get(`${API_ROOT}/:${id}`)
-            .then((response) => {
-                employee = new Employee(
-                    response.data.id,
-                    response.data.name,
-                    response.data.email
-                );
-
-                console.log(`Successfully got employee of id: ${id}`);
-            })
-            .catch((err) => {
-                console.log(`Error getting Employee with an id of: ${id} ` + err);
-            });
-
-        return employee;
-    };
-
-    static async getAllForBusiness(businessId: number) {
-        //biz id might be stored in the store... adjust later maybe...
-        let results: Employee[] = [];
-
-        await apiClient
-            .get(`${API_ROOT}/:${businessId}`)
-            .then((response) => {
-                results = response.data.map((d) =>{
-                    d.id,
-                    d.name,
-                    d.email
-                });
-            })
-            .catch((err) => {
-                console.log(`Error getting all Employees for bussines ${businessId} ` + err);
-            });
-    };
+    static async getAllForBusiness(businessID: number) {
+        return await DatabaseServices.get<Employee[]>(
+            `${API_ROOT}/business/:${businessID}`,
+        );
+    }
 }
-
