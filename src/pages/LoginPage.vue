b0d37ec49f73d96ea3099dd9461bf7eb52b4a4e5
Maximus253 <m.jones@eagles.oc.edu>
2026-03-04 14:01:56 -0600

    puter, lockin

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,26 +1,28 @@
-<script setup>
-import { ref } from 'vue';
-import type { ButtonProps } from '@nuxt/ui'
+<script setup lang="ts">
+import { shallowRef } from "vue";
+import type { ButtonProps } from "@nuxt/ui";
 
-const providers =
-    ref <
-    ButtonProps >
-    [
-        {
-            label: "Google",
-            icon: "i-simple-icons-google",
-            color: "neutral",
-            variant: "subtle",
-        },
-    ];
+const providers = shallowRef<ButtonProps[]>([
+    {
+        label: "Google",
+        icon: "i-lucide-google",
+        color: "neutral",
+        variant: "subtle",
+    }
+]);
+
+AuthSer
 </script>
+
 <template>
-    <div>
-        <UAuthForm
-            title="Login"
-            description="Please enter your Oklahoma Christian credentials to continue."
-            icon="i-lucide-user"
-            :providers="providers"
-        />
+    <div class="flex flex-col items-center justify-center gap-4 p-4">
+        <UPageCard class="w-full max-w-md">
+            <UAuthForm
+                title="Login"
+                description="Please enter your Oklahoma Christian credentials to continue."
+                icon="i-lucide-user"
+                :providers="providers"
+            />
+        </UPageCard>
     </div>
 </template>
