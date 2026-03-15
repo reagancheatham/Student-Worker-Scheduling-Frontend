c4f9fa9e99ec0057941e30ef076ca9c80191d93c
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2026-03-15 05:08:01 -0500

    bro idk ive screwed it

--- src/pages/EmployeesPage.vue
+++ src/pages/EmployeesPage.vue
@@ -3,16 +3,18 @@ import { ref } from "vue";
 import { Employee } from "@classes/database/employee";
 import { EmployeeServices } from "../services/employeeServices";
 import { TableColumn } from "@nuxt/ui";
-import { h, resolveComponent } from 'vue';
-import { useClipboard } from '@vueuse/core';
+import { h, resolveComponent } from "vue";
+import { useClipboard } from "@vueuse/core";
 import { Row } from "@tanstack/vue-table";
 import { f } from "vue-router/dist/router-CWoNjPRp.mjs";
 
 const globalFilter = ref();
 const toast = useToast();
 const { copy } = useClipboard();
-const UButton = resolveComponent('UButton');
-const UDropdownMenu = resolveComponent('UDropdownMenu');
+const UButton = resolveComponent("UButton");
+const UDropdownMenu = resolveComponent("UDropdownMenu");
+
+const isOpen = ref(false); //ask rag for better ways to handle the double confirm modal???
 
 let data = ref<Employee[]>([]);
 
@@ -39,69 +41,78 @@ const columns: TableColumn<Employee>[] = [
         cell: ({ row }) => row.original.formattedPhoneNumber,
     },
     {
-        id: 'actions',
+        id: "actions",
         meta: {
             class: {
-                td: 'text-right'
-            }
+                td: "text-right",
+            },
         },
         cell: ({ row }) => {
             return h(
                 UDropdownMenu,
                 {
                     content: {
-                        align: 'end'
+                        align: "end",
                     },
                     items: getRowItems(row),
                 },
                 () =>
                     h(UButton, {
-                        icon: 'i-lucide-ellipsis-vertical',
-                        color: 'neutral',
-                        variant: 'ghost',
-                    })
-            )
-        }
-    }
+                        icon: "i-lucide-ellipsis-vertical",
+                        color: "neutral",
+                        variant: "ghost",
+                    }),
+            );
+        },
+    },
 ];
 
 function getRowItems(row: Row<Employee>) {
     return [
-        {
-            label: 'Copy Student ID',
-            icon: 'i-lucide-copy',
-            onSelect() {
-                copy(row.original.studentID);
-
-                toast.add({
-                    title: 'Employee ID copied to clipboard!',
-                    color: 'success',
-                    icon: 'i-lucide-circle-check'
-                })
-            }
-        },
-        {
-            label: 'Copy Email',
-            icon: 'i-lucide-copy',
-            onselect() {
-                copy(row.original.email);
-
-                toast.add({
-                    title: 'Employee ID copied to clipboard!',
-                    color: 'success',
-                    icon: 'i-lucide-circle-check'
-                })
-            }
-        },
-        {
-            type: 'separator'
-        },
-        {
-            label: 'Delete',
-            icon: 'i-lucide-trash',
-            color: 'error'
-        }
-    ]
+        [
+            {
+                label: "Copy Student ID",
+                icon: "i-lucide-copy",
+                onSelect() {
+                    copy(row.original.studentID);
+    
+                    toast.add({
+                        title: "Employee ID copied to clipboard!",
+                        color: "success",
+                        icon: "i-lucide-circle-check",
+                    })
+                },
+            },
+        ],
+        [
+            {
+                label: "Copy Email",
+                icon: "i-lucide-copy",
+                onselect() {
+                    copy(row.original.email);
+    
+                    toast.add({
+                        title: "Employee Email copied to clipboard!",
+                        color: "success",
+                        icon: "i-lucide-circle-check",
+                    })
+                },
+            },
+        ],
+        [
+            {
+                type: "separator",
+            },
+            {
+                label: "Delete",
+                icon: "i-lucide-trash",
+                color: "error",
+                onselect() {
+                    console.log("made it");
+                },
+            },
+        ]
+    ];
 }
 
 EmployeeServices.getAllForBusiness(1)
@@ -115,6 +126,14 @@ EmployeeServices.getAllForBusiness(1)
 </script>
 
 <template>
+    <!-- <UModal v-model="isOpen">
+        <UCard>
+            <template #header>
+                <h3 class="text-lg font-semibold">Confirm Deletion</h3>
+            </template>
+        </UCard>
+    </UModal> -->
+
     <div class="h-screen flex flex-col">
         <div class="flex px-4 py-3.5 border-b border-accented">
             <UInput
@@ -123,7 +142,13 @@ EmployeeServices.getAllForBusiness(1)
                 placeholder="Filter..."
             />
         </div>
-        <UTable class="flex-1" :columns="columns" :data="data" ref="table" v-model:global-filter="globalFilter" >
+        <UTable
+            class="flex-1"
+            :columns="columns"
+            :data="data"
+            ref="table"
+            v-model:global-filter="globalFilter"
+        >
             <template #name-cell="{ row }">
                 <div class="flex items-center gap-3">
                     <UAvatar />
@@ -134,6 +159,6 @@ EmployeeServices.getAllForBusiness(1)
                     </div>
                 </div>
             </template>
-        </UTable> 
+        </UTable>
     </div>
 </template>
