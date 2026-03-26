db8eecaee16fbc469200195d782b652e3dc83882
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2026-03-26 15:32:14 -0500

    util function for mobile

--- src/components/templates/NavbarLayout.vue
+++ src/components/templates/NavbarLayout.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
-    import type { NavigationMenuItem } from '@nuxt/ui'
+import type { NavigationMenuItem } from "@nuxt/ui";
+import { ref, onMounted, onUnmounted } from "vue";
+import { isMobileApp } from "@classes/util/isMobile";
 
-    const isMobile = window.innerWidth <= 768;
+const isMobile = isMobileApp();
 
-    const items: NavigationMenuItem[] = [
-        {
-            label: 'Home',
-            icon: 'i-lucide-house'
-        },
-        {
-            label: 'Schedule',
-            icon: 'i-lucide-calendar-fold'
-        },
-        {
-            label: 'Shift Board',
-            icon: 'i-lucide-clipboard-list'
-        },
-    ]
+const items: NavigationMenuItem[] = [
+    {
+        label: "Home",
+        icon: "i-lucide-house",
+    },
+    {
+        label: "Schedule",
+        icon: "i-lucide-calendar-fold",
+    },
+    {
+        label: "Shift Board",
+        icon: "i-lucide-clipboard-list",
+    },
+];
 </script>
 
 <template>
@@ -35,15 +37,15 @@
     </UDashboardGroup>
 
     <UNavigationMenu
-    v-if="isMobile"
-    :items="items"
-    :ui="{
-      root: 'justify-around border-t border-default py-2',
-      item: 'py-0',
-      link: 'flex-col gap-1 px-6',
-      linkLeadingIcon: 'size-5',
-      linkLabel: 'text-[10px]/3 font-normal'
-    }"
-    class="w-full"
-  />
+        v-if="isMobile"
+        :items="items"
+        :ui="{
+            root: 'justify-around border-t border-default py-2',
+            item: 'py-0',
+            link: 'flex-col gap-1 px-6',
+            linkLeadingIcon: 'size-5',
+            linkLabel: 'text-[10px]/3 font-normal',
+        }"
+        class="w-full fixed bottom-0"
+    />
 </template>
