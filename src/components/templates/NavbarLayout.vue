be2401a33b0320c1488d30ca3aa3fd0a04cba962
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2026-03-26 18:17:19 -0500

    topbar

--- src/components/templates/NavbarLayout.vue
+++ src/components/templates/NavbarLayout.vue
@@ -21,6 +21,15 @@ const isMobile = isMobileApp();
     </UDashboardGroup>
 
     <div class="flex flex-col min-h-screen" v-if="isMobile">
+        <UHeader :toggle="false">
+            <template #left>
+                <img class="h-10 w-65" src="/mobileOC.png" alt="OC Logo" />
+            </template>
+            <template #right>
+                <UAvatar alt="Davey Clonts" />
+            </template>
+        </UHeader>
+
         <div class="flex-1">
             <RouterView />
         </div>
