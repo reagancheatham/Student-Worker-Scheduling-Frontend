b5410a52407de7a306370c599e94c83d05a43ea6
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2026-03-25 22:59:21 -0500

    mobile layout wip

--- src/components/templates/NavbarLayout.vue
+++ src/components/templates/NavbarLayout.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+    import type { NavigationMenuItem } from '@nuxt/ui'
+
+    const isMobile = window.innerWidth <= 768;
+
+    const items: NavigationMenuItem[] = [
+        {
+            label: 'Home',
+            icon: 'i-lucide-house'
+        },
+        {
+            label: 'Schedule',
+            icon: 'i-lucide-calendar-fold'
+        },
+        {
+            label: 'Shift Board',
+            icon: 'i-lucide-clipboard-list'
+        },
+    ]
+</script>
+
 <template>
-    <UDashboardGroup>
+    <UDashboardGroup v-if="!isMobile">
         <ScheduleSidebar />
         <UDashboardPanel>
             <template #header>
@@ -12,4 +33,17 @@
             </template>
         </UDashboardPanel>
     </UDashboardGroup>
+
+    <UNavigationMenu
+    v-if="isMobile"
+    :items="items"
+    :ui="{
+      root: 'justify-around border-t border-default py-2',
+      item: 'py-0',
+      link: 'flex-col gap-1 px-6',
+      linkLeadingIcon: 'size-5',
+      linkLabel: 'text-[10px]/3 font-normal'
+    }"
+    class="w-full"
+  />
 </template>
