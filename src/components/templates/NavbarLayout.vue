df4b0007019632e979ca231d10f97e9b243de35e
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2026-03-26 17:25:35 -0500

    mobile tab migration

--- src/components/templates/NavbarLayout.vue
+++ src/components/templates/NavbarLayout.vue
@@ -1,24 +1,8 @@
 <script setup lang="ts">
-import type { NavigationMenuItem } from "@nuxt/ui";
-import { ref, onMounted, onUnmounted } from "vue";
 import { isMobileApp } from "@classes/util/isMobile";
+import MobileTabs from "@components/MobileTabs.vue";
 
 const isMobile = isMobileApp();
-
-const items: NavigationMenuItem[] = [
-    {
-        label: "Home",
-        icon: "i-lucide-house",
-    },
-    {
-        label: "Schedule",
-        icon: "i-lucide-calendar-fold",
-    },
-    {
-        label: "Shift Board",
-        icon: "i-lucide-clipboard-list",
-    },
-];
 </script>
 
 <template>
@@ -41,16 +25,6 @@ const items: NavigationMenuItem[] = [
             <RouterView />
         </div>
 
-        <UNavigationMenu
-            :items="items"
-            :ui="{
-                root: 'justify-around border-t border-default py-2',
-                item: 'py-0',
-                link: 'flex-col gap-1 px-6',
-                linkLeadingIcon: 'size-5',
-                linkLabel: 'text-[10px]/3 font-normal',
-            }"
-            class="w-full fixed bottom-0"
-        />
+        <MobileTabs />
     </div>
 </template>
