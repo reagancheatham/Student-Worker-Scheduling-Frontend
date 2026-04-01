b298491f71f0014af4b7176065d7d558da6a59a4
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2026-03-31 20:32:40 -0500

    User welcome

--- src/mobile/pages/HomePage.vue
+++ src/mobile/pages/HomePage.vue
@@ -1,3 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import { ref } from 'vue';
 
-<template>HomePage</template>
+    const user = ref(JSON.parse(localStorage.getItem("user")));
+    console.log(user);
+</script>
+
+<template>
+    <div class="pl-6 pt-6 font-bold w-full text-xl">
+        Welcome back, {{user?.firstName}}
+    </div>
+</template>
