2be4aad9267fc1b7e397cee8c6fc3e76753f2329
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2026-04-01 20:19:32 -0500

    wip

--- src/mobile/pages/HomePage.vue
+++ src/mobile/pages/HomePage.vue
@@ -9,4 +9,22 @@
     <div class="pl-6 pt-6 font-bold w-full text-xl">
         Welcome back, {{user?.firstName}}
     </div>
+    <div class="flex flex-col gap-1 pt-6 pl-10">
+        <UBadge size="lg" color="primary" class="w-75">
+            Your Next Shift Starts Soon
+        </UBadge>
+        <div class="flex flex-row items-end gap-3">
+            <UIcon name="i-lucide-calendar" class="size-5"/>
+            <div>
+                Thursday, July 25th
+            </div>
+        </div>
+        <div class="flex flex-row items-end gap-3">
+            <UIcon name="i-lucide-clock" class="size-5" />
+            <div>
+                9:00 A.M. - 2:00 P.M.
+            </div>
+        </div>
+    </div>
+    
 </template>
