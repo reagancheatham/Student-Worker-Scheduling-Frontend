c6a89b361a0db9b9136928884e7b28ee8d9b7261
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2026-04-13 21:59:05 -0500

    wip

--- src/mobile/pages/TradeBoardPage.vue
+++ src/mobile/pages/TradeBoardPage.vue
@@ -1,3 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    
+</script>
 
-<template>Trade Board</template>
+<template>
+    <div class="text-center pt-5 font-bold text-xl">
+        Trade Board
+    </div>
+
+    <div class="overflow-y-auto px-1">
+        <div class="p-3 space-y-3">
+            <UPageCard orientation="horizontal">
+                <div>
+                    Test
+                </div>
+            </UPageCard>
+        </div>
+    </div>
+</template>
