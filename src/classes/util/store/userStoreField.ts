83709a772dd59f3ab3068a69c18ca7efaefc11f7
reagancheatham <122314281+reagancheatham@users.noreply.github.com>
2026-04-19 18:35:53 -0500

    Fix store calls (#78)

--- src/classes/util/store/userStoreField.ts
+++ src/classes/util/store/userStoreField.ts
@@ -37,4 +37,28 @@ export class UserStoreField extends StoreField<User> {
     public clear(): void {
         localStorage.removeItem(STORE_KEY);
     }
+
+    public getImmediate(): User | undefined {
+        const storedUser = localStorage.getItem(STORE_KEY);
+
+        if (storedUser && storedUser != "undefined") {
+            try {
+                const user = JSON.parse(storedUser);
+                return new User(
+                    user.id,
+                    user.studentID,
+                    user.permissionRoleID,
+                    user.firstName,
+                    user.lastName,
+                    user.email,
+                    user.phoneNumber,
+                    user.token,
+                    user.profilePicture,
+                );
+            } catch (error) {
+                console.error(`Error parsing user from localStorage: ${error}`);
+                return undefined;
+            }
+        }
+    }
 }
