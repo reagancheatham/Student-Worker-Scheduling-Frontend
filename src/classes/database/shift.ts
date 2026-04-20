e5b89871c7fcf083747f6ca9f291b74735f88cdc
Maximus253 <122314662+Maximus253@users.noreply.github.com>
2026-04-19 23:10:01 -0500

    Mj create notification tab (#75)
    * reagan question
    
    * another reagan question
    
    * Added notification tables
    
    * Notification should be done

--- src/classes/database/shift.ts
+++ src/classes/database/shift.ts
@@ -29,7 +29,7 @@ export class Shift extends DatabaseModel {
             ? EventColor.fromString(data["color"])
             : EventColor.blue;
         const employee = data["Employee"]
-            ? Employee.create(data["Employee"])
+            ? Employee.createFromData(data["Employee"])
             : undefined;
 
         return new Shift(
