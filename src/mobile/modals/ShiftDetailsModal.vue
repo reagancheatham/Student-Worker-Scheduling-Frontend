39340ed66cad7ab9cbf2a1e6d0b3ec9e4ed3c9a0
DaveyClonts <122319800+DaveyClonts@users.noreply.github.com>
2026-04-19 22:46:11 -0500

    shift details modal

---
+++ src/mobile/modals/ShiftDetailsModal.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { DateFormatter } from "@classes/util/dateFormatter";
+
+defineProps<{
+    shift: any; //sorry rag
+    name: string;
+    isTrade: boolean;
+}>();
+</script>
+
+<template>
+    <UModal :title="shift.fullName">
+        <template #body>
+            <div class="space-y-1">
+                {{ shift.employeeMessage }}
+    
+                <div class="flex flex-row items-end gap-3 pt-3">
+                    <UIcon name="i-lucide-calendar" class="size-5" />
+                    <div>
+                        {{ DateFormatter.weekday(shift.startTime) }}
+                        {{ DateFormatter.dateFormatted(shift.startTime) }}
+                    </div>
+                </div>
+                <div class="flex flex-row items-end gap-3">
+                    <UIcon name="i-lucide-clock" class="size-5" />
+                    <div>
+                        {{
+                            DateFormatter.shiftTime(shift.startTime, shift.endTime)
+                        }}
+                    </div>
+                </div>
+                <div class="flex flex-row items-end gap-3">
+                    <UIcon name="i-lucide-building-2" class="size-5" />
+                    <div>{{ name }}</div>
+                </div>
+                <div class="flex justify-center gap-10 pt-3">
+                    <UButton
+                        class="w-20 justify-center"
+                        label="Accept"
+                        size="xl"
+                    ></UButton>
+                    <UButton
+                        class="w-20 justify-center"
+                        label="Deny"
+                        size="xl"
+                        v-if="isTrade === true"
+                    ></UButton>
+                </div>
+            </div>
+        </template>
+    </UModal>
+</template>
--- src/mobile/pages/TradeBoardPage.vue
+++ src/mobile/pages/TradeBoardPage.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import { TradeServices } from '../../services/tradeServices';
+import { Component, onMounted, ref } from "vue";
+import { TradeServices } from "../../services/tradeServices";
 import { Store } from "@classes/util/store.ts";
-import { ShiftTradeRequest } from '@classes/database/shiftTradeRequests';
-import { ShiftOfferRequest } from '@classes/database/shiftOfferRequests';
-import { Business } from '@classes/database/business';
-import { DateFormatter } from '../../classes/util/dateFormatter';
-import { ApprovalStatus } from '@classes/util/approvalStatus';
+import { ShiftTradeRequest } from "@classes/database/shiftTradeRequests";
+import { ShiftOfferRequest } from "@classes/database/shiftOfferRequests";
+import { Business } from "@classes/database/business";
+import { DateFormatter } from "../../classes/util/dateFormatter";
+import { ApprovalStatus } from "@classes/util/approvalStatus";
+import ShiftDetailsModal from "../modals/ShiftDetailsModal.vue";
+import { Overlay } from "@nuxt/ui/runtime/composables/useOverlay.js";
 
 const business = ref<Business>();
 const tradeRequests = ref<ShiftTradeRequest[]>([]);
@@ -14,7 +16,8 @@ const offerRequests = ref<ShiftOfferRequest[]>([]);
 const pendingTradeRequests = ref<ShiftTradeRequest[]>([]);
 const pendingOfferRequests = ref<ShiftOfferRequest[]>([]);
 
-//const dateFormatter = new DateFormatter();
+const overlay = useOverlay();
+const modal = overlay.create(ShiftDetailsModal);
 
 console.log("trades", tradeRequests);
 console.log("offers", offerRequests);
@@ -23,6 +26,15 @@ onMounted(async () => {
     business.value = await Store.getBusiness();
     await loadData();
 });
+
+
+async function openShiftModal(shiftData, isTrade) {
+    modal.open({
+        shift: shiftData,
+        name: business.value.name,
+        isTrade: isTrade
+    });
+}
 
 const testTrades = [
     new ShiftTradeRequest(
@@ -60,7 +72,7 @@ const testTrades = [
         new Date(Date.now() + 7 * 24 * 60 * 60 * 1000 + 8 * 60 * 60 * 1000),
         "Mike",
         "Johnson",
-    )
+    ),
 ];
 
 const testOffers = [
@@ -74,7 +86,7 @@ const testOffers = [
         new Date(Date.now() + 24 * 60 * 60 * 1000), // tomorrow
         new Date(Date.now() + 32 * 60 * 60 * 1000), // tomorrow + 8h
         "Alice",
-        "Brown"
+        "Brown",
     ),
     new ShiftOfferRequest(
         2,
@@ -86,7 +98,7 @@ const testOffers = [
         new Date(Date.now() + 2 * 24 * 60 * 60 * 1000), // day after tomorrow
         new Date(Date.now() + 2 * 24 * 60 * 60 * 1000 + 8 * 60 * 60 * 1000),
         "Bob",
-        "Wilson"
+        "Wilson",
     ),
     new ShiftOfferRequest(
         3,
@@ -98,8 +110,8 @@ const testOffers = [
         new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // next week
         new Date(Date.now() + 7 * 24 * 60 * 60 * 1000 + 8 * 60 * 60 * 1000),
         "Carol",
-        "Davis"
-    )
+        "Davis",
+    ),
 ];
 
 const tabs = [
@@ -115,10 +127,16 @@ const tabs = [
 
 async function loadData() {
     try {
-        tradeRequests.value = await TradeServices.getAllAvailableTradeRequests(business.value.id);
-        offerRequests.value = await TradeServices.getAllAvailableOfferedShifts(business.value.id);
-        pendingTradeRequests.value = await TradeServices.getAllPendingTradeRequests(business.value.id);
-        pendingOfferRequests.value = await TradeServices.getAllPendingOfferedShifts(business.value.id);
+        tradeRequests.value = await TradeServices.getAllAvailableTradeRequests(
+            business.value.id,
+        );
+        offerRequests.value = await TradeServices.getAllAvailableOfferedShifts(
+            business.value.id,
+        );
+        pendingTradeRequests.value =
+            await TradeServices.getAllPendingTradeRequests(business.value.id);
+        pendingOfferRequests.value =
+            await TradeServices.getAllPendingOfferedShifts(business.value.id);
 
         //TODO: sort pending data so that approved and denied shifts are on top
     } catch (err) {
@@ -130,43 +148,86 @@ async function loadData() {
 <template>
     <UTabs :items="tabs">
         <template #trade>
-            <div class="text-center pt-5 pb-3 font-bold text-xl border-b-1 border-gray-300">
+            <div
+                class="text-center pt-5 pb-3 font-bold text-xl border-b-1 border-gray-300"
+            >
                 Trade Board
             </div>
 
             <div class="flex h-screen">
                 <div class="flex-1 overflow-y-auto px-1 pb-60">
                     <div class="p-3 space-y-3">
-                        <UPageCard v-for="shift in testTrades" orientation="horizontal" class="border-4 border-maroon-500">
-                            <div class="flex justify-between items-center">
-                                <div class="font-bold">
-                                    {{ shift.fullName }} 
-                                </div>
-                                <div>
-                                    {{ DateFormatter.dateFormatted(shift.startTime) }}
-                                </div>
-                            </div>
-                            <div>
-                                {{ shift.employeeMessage }}
-                            </div>
-                            <div class="flex items-center justify-center font-bold">
-                                {{ DateFormatter.startTimeFormatted(shift.startTime) }} - {{ DateFormatter.endTimeFormatted(shift.endTime) }}
-                            </div>
-                        </UPageCard>
-                        <UPageCard v-for="shift in testOffers" orientation="horizontal">
+                        <UPageCard
+                            v-for="shift in testTrades"
+                            orientation="horizontal"
+                            class="border-4 border-maroon-500"
+                            @click="openShiftModal(shift, true)"
+                        >
                             <div class="flex justify-between items-center">
                                 <div class="font-bold">
                                     {{ shift.fullName }}
                                 </div>
                                 <div>
-                                    {{ DateFormatter.dateFormatted(shift.startTime) }}
+                                    {{
+                                        DateFormatter.dateFormatted(
+                                            shift.startTime,
+                                        )
+                                    }}
                                 </div>
                             </div>
                             <div>
                                 {{ shift.employeeMessage }}
                             </div>
-                            <div class="flex items-center justify-center font-bold">
-                                {{ DateFormatter.startTimeFormatted(shift.startTime) }} - {{ DateFormatter.endTimeFormatted(shift.endTime) }}
+                            <div
+                                class="flex items-center justify-center font-bold"
+                            >
+                                {{
+                                    DateFormatter.startTimeFormatted(
+                                        shift.startTime,
+                                    )
+                                }}
+                                -
+                                {{
+                                    DateFormatter.endTimeFormatted(
+                                        shift.endTime,
+                                    )
+                                }}
+                            </div>
+                        </UPageCard>
+                        <UPageCard
+                            v-for="shift in testOffers"
+                            orientation="horizontal"
+                            @click="openShiftModal(shift, false)"
+                        >
+                            <div class="flex justify-between items-center">
+                                <div class="font-bold">
+                                    {{ shift.fullName }}
+                                </div>
+                                <div>
+                                    {{
+                                        DateFormatter.dateFormatted(
+                                            shift.startTime,
+                                        )
+                                    }}
+                                </div>
+                            </div>
+                            <div>
+                                {{ shift.employeeMessage }}
+                            </div>
+                            <div
+                                class="flex items-center justify-center font-bold"
+                            >
+                                {{
+                                    DateFormatter.startTimeFormatted(
+                                        shift.startTime,
+                                    )
+                                }}
+                                -
+                                {{
+                                    DateFormatter.endTimeFormatted(
+                                        shift.endTime,
+                                    )
+                                }}
                             </div>
                         </UPageCard>
                     </div>
@@ -174,7 +235,9 @@ async function loadData() {
             </div>
         </template>
         <template #pending>
-            <div class="text-center pt-5 pb-3 font-bold text-xl border-b-1 border-gray-300">
+            <div
+                class="text-center pt-5 pb-3 font-bold text-xl border-b-1 border-gray-300"
+            >
                 Pending Approval
             </div>
             <div class="flex h-screen">
@@ -184,28 +247,41 @@ async function loadData() {
                             <div class="grid grid-cols-4">
                                 <div class="col-span-3">
                                     <div class="font-bold">
-                                        {{ DateFormatter.dateFormatted(shift.startTime) }}
+                                        {{
+                                            DateFormatter.dateFormatted(
+                                                shift.startTime,
+                                            )
+                                        }}
                                     </div>
                                     <div>
-                                        Swaping with {{ shift.fullName }}, {{ DateFormatter.shiftTime(shift.startTime, shift.endTime) }}
+                                        Swaping with {{ shift.fullName }},
+                                        {{
+                                            DateFormatter.shiftTime(
+                                                shift.startTime,
+                                                shift.endTime,
+                                            )
+                                        }}
                                     </div>
-                                    <div v-if="shift.status === 'Pending'" class="flex items-center gap-2 text-warning">
+                                    <div
+                                        v-if="shift.status === 'Pending'"
+                                        class="flex items-center gap-2 text-warning"
+                                    >
                                         <UIcon name="i-lucide-clock" />
-                                        <span>
-                                            Pending Approval...
-                                        </span>
+                                        <span> Pending Approval... </span>
                                     </div>
-                                    <div v-if="shift.status === 'Approved'" class="flex items-center gap-2 text-success">
+                                    <div
+                                        v-if="shift.status === 'Approved'"
+                                        class="flex items-center gap-2 text-success"
+                                    >
                                         <UIcon name="i-lucide-circle-check" />
-                                        <span>
-                                            Swap Approved!
-                                        </span>
+                                        <span> Swap Approved! </span>
                                     </div>
-                                    <div v-if="shift.status === 'Denied'" class="flex items-center gap-2 text-error">
+                                    <div
+                                        v-if="shift.status === 'Denied'"
+                                        class="flex items-center gap-2 text-error"
+                                    >
                                         <UIcon name="i-lucide-circle-x" />
-                                        <span>
-                                            Swap Request Denied...
-                                        </span>
+                                        <span> Swap Request Denied... </span>
                                     </div>
                                 </div>
                             </div>
