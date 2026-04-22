0d048fbd5cec5177c907f685b3f398bb83c701b6
reagancheatham <reagan.cheatham@eagles.oc.edu>
2026-04-22 10:25:33 -0500

    Convert strings to iso

--- src/services/shiftServices.ts
+++ src/services/shiftServices.ts
@@ -39,8 +39,8 @@ export class ShiftServices {
         startTime: Date,
         endTime: Date,
     ) {
-        const startString = `${startTime.getMonth() + 1}-${startTime.getDate()}-${startTime.getFullYear()}-${startTime.getHours()}:${startTime.getMinutes()}`;
-        const endString = `${endTime.getMonth() + 1}-${endTime.getDate()}-${endTime.getFullYear()}-${endTime.getHours()}:${endTime.getMinutes()}`;
+        const startString = encodeURIComponent(startTime.toISOString());
+        const endString = encodeURIComponent(endTime.toISOString());
 
         return await DatabaseServices.getAll<Shift>(
             Shift,
